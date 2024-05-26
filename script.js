414a2f47ed4019c2617a16ddebc4b8fea86cda15
Arsh Ahmed <arshahmed575@gmail.com>
2024-05-27 04:19:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,106 @@
+score = JSON.parse(localStorage.getItem('score')) || {
+  wins:0,
+  losses:0,
+  ties:0
+};
+
+
+function computerMove(){
+
+  let computerChoice = '';
+
+  const randomInt = Math.random();
+
+  if(randomInt<1/3){
+    computerChoice = 'rock';
+  }
+  else if(randomInt>=1/3 && randomInt<2/3){
+    computerChoice = 'paper';
+  }
+  else{
+    computerChoice = 'scissors';
+  }
+
+  return computerChoice;
+
+}
+
+function playGame(userMove){
+
+  const opp = computerMove();
+  let result = '';
+
+  if(userMove === 'rock'){
+
+    if(opp === 'rock'){
+      result = 'tie';
+    }
+    else if(opp === 'paper'){
+      result = 'you lose';
+    }
+    else{
+      result = 'you win';
+    }
+
+  }
+  else if(userMove === 'paper'){
+
+    if(opp === 'paper'){
+      result = 'tie';
+    }
+    else if(opp === 'scissors'){
+      result = 'you lose';
+    }
+    else{
+      result = 'you win';
+    }
+
+  }
+
+  else{
+    if(opp === 'scissors'){
+      result = 'tie';
+    }
+    else if(opp === 'rock'){
+      result = 'you lose';
+    }
+    else{
+      result = 'you win';
+    }
+  }
+
+
+  if(result === 'you win'){
+    score.wins++;
+  }
+  else if(result === 'you lose'){
+    score.losses++;
+  }
+  else{
+    score.ties++;
+  }
+
+  
+ localStorage.setItem('score',JSON.stringify(score));
+
+  
+
+  alert(`You chose ${userMove}, Computer chose ${opp}
+    ${result}
+    Wins:${score.wins},Losses:${score.losses},Ties:${score.ties}
+  `)
+
+}
+
+function resetScore(){
+
+  console.log('resetting the score....')
+
+  score.wins = 0;
+  score.losses = 0;
+  score.ties = 0;
+  localStorage.removeItem('score');
+
+  console.log(score)
+}
+
